7b37b162ef49f112ceea020607c313d13b70097b
Daniel Hampikian <danielhampikian@gmail.com>
2019-03-31 11:18:43 -0600

    fixed location object issue

--- MondayMapApp/src/app/home/home.page.ts
+++ MondayMapApp/src/app/home/home.page.ts
@@ -1,46 +1,45 @@
-import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 declare var google;
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage implements OnInit{
 
-@ViewChild('map') mapElement: ElementRef;
-location: Location = {
-  latitude: 0,
-  longitude: 0
-}
-map: any;
-latlng: any;
+  @ViewChild('map') mapElement: ElementRef;
 
-constructor(private geolocation: Geolocation) {
-  this.latlng = new google.maps.LatLng(-5, 5);
- }
+  map: any;
+  latlng: any;
 
-ngOnInit() {
-    let mapOptions = {
-      zoom: 10,
-      center: this.latlng,
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      mapTypeControl: false,
-      streetViewControl: false,
-      fullScreenControl: false
+    constructor(private geolocation: Geolocation){
     }
-    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-}
-getGeoLocation(){
-  this.geolocation.getCurrentPosition().then(pos => {
-  this.location.latitude = pos.coords.latitude;
-  this.location.longitude = pos.coords.longitude;
-    }).catch((error) => {
-      console.log('Error getting location', error);
-    });
-    this.latlng = new google.maps.LatLng(this.location.latitude, this.location.longitude);
-    this.map.setCenter(this.latlng);
-}
+
+    ngOnInit(){
+      this.latlng = new google.maps.LatLng(-5,5);
+
+      let mapOptions = {
+        zoom: 10,
+        center: this.latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        mapTypeControl: false,
+        streetViewControl: false,
+        fullScreenControl: false
+      }
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    }
+    getGeolocation(){
+      this.geolocation.getCurrentPosition().then(pos => {
+        this.latlng = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+        this.map.setCenter(this.latlng);
+          }).catch((error) => {
+            console.log('Error getting location', error);
+          });
+          
+      }
+    
 }
